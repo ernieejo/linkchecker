9cbbb671c0581d41e71325ca19d7921f950a9c01
Erin Johnson <36278045+ernieejo@users.noreply.github.com>
2022-09-28 07:51:28 -0400

    Update link_checker_script.py

--- link_checker_script.py
+++ link_checker_script.py
@@ -10,15 +10,17 @@ from urllib3.exceptions import InsecureRequestWarning
 from urllib3 import disable_warnings
 disable_warnings(InsecureRequestWarning)
 
-pid_loop = []
 url = ["url"]
 pid = ["pid"]
 Status = []
+inputfile = ['C:\\...urls.csv'] #enter the path to your input file
+outputfile = ['C:\\...statuscodes.csv] #enter the path to your outputfile]
 
-cols = ['pid', 'url', 'Status']
-df = pd.read_csv('inputfile')
+cols = ['pid', 'url', 'Status'] #define column headers
+df = pd.read_csv('inputfile') #open input file
 print(df)
 
+#get the url status code
 def url_access(x):
     try:
         return requests.head(x,timeout=5).status_code
@@ -30,5 +32,5 @@ df['Status'] = df['url'].apply(url_access)
 dfcount = df.groupby('Status')['url'].count().reset_index()
 
 print(df)
-df.to_csv('outputfile')
+df.to_csv('outputfile') #create outputfile with status codes
 
